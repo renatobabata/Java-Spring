210e8df3c79c4161c8ef7a625192669ab4865a80
Renato Babata <babatarenato@gmail.com>
2020-01-26 17:27:24 -0300

    Java - Spring tutorial

---
+++ spring5-tutorial/src/main/java/com/renato/spring5tutorial/Spring5TutorialApplication.java
@@ -0,0 +1,13 @@
+package com.renato.spring5tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5TutorialApplication.class, args);
+	}
+
+}
---
+++ spring5-tutorial/src/test/java/com/renato/spring5tutorial/Spring5TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.spring5tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5TutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
